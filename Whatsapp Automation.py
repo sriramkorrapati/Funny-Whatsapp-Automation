38ca9f9063c76cb1ee7f5364717a5787112004e3
sriram korrapati <102807942+sriramkorrapati@users.noreply.github.com>
2022-06-09 11:11:34 +0530

    Add files via upload

--- Whatsapp Automation.py
+++ Whatsapp Automation.py
@@ -1,24 +1,65 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 import pyautogui as pag
 
 
-name = input("Enter Contact Name: ")
-limit = int(input("Count: "))
-msg = input("Message: ")
+def userinputs():
+    try:
+        global name, limit, msg
+        name = input("Enter Contact Name:")
+        limit = int(input("Count: "))
+        msg = input("Message: ")
+    except:
+        print("Unexpected Error in Receiving Data")
 
-driver = webdriver.Chrome(executable_path="/Applications/chromedriver")
-driver.maximize_window()
-driver.get("https://web.whatsapp.com")
-driver.implicitly_wait(120)
 
-driver.find_element_by_xpath('//span[@title = "{}"]'.format(name)).click();
-driver.find_element_by_class_name("p3_M1").click();
+def callbrowser():
+    try:
+        global driver
+        driver = webdriver.Firefox(
+            service=Service(GeckoDriverManager().install()))
+        driver.maximize_window()
+        driver.get("https://web.whatsapp.com")
+        driver.implicitly_wait(1000)
+        # time.sleep(20)
+    except:
+        print("Driver Problem")
 
-i = 0
-driver.implicitly_wait(5)
-while i < int(limit):
-    pag.typewrite(msg)
-    pag.press('Enter')
-    i += 1
 
-input("Press Enter To Exit")
+def callsearchbar():
+    try:
+        element = driver.find_element_by_class_name("_28-cz").click()
+        time.sleep(1)
+        pag.typewrite(name)
+        time.sleep(1)
+        pag.press('Enter')
+        time.sleep(2)
+    except:
+        print("search bar problem")
+
+
+def messagelooping():
+    try:
+        message = driver.find_element_by_class_name("p3_M1").click()
+        i = 0
+        driver.implicitly_wait(5)
+        while i < int(limit):
+            pag.typewrite(msg)
+            pag.press('Enter')
+            i += 1
+        time.sleep(10)
+    except:
+        print("Message Sending Problem")
+
+
+userinputs()
+callbrowser()
+callsearchbar()
+messagelooping()
+
+
+driver.close()
+
+# Developed By "Sriram_Korrapati"
